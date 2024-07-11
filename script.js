e5e1146d4e890ac9f32853cf7e3359782060929a
SriMoorthy <srimoorthy27@gmail.com>
2024-07-11 16:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let Level = document.getElementById('level')
+let N=Level.innerHTML.toString()
+let n= parseInt(N[N.length-1]-1)
+alert(`The level of the element is: ${n}`)
